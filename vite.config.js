86be65da76c1e3d877241af5673f2743d2ba00b6
ashirwad5555 <ashirwadkatkamwar@gmail.com>
2026-04-04 00:28:08 +0530

    fourth

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zorvyan/',
+  base: "/zorvyan/",
   plugins: [react()],
-})
+});
